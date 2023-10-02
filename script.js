da63b89badb5322f15fe2248c871d5c6aee22f8f
sandra prinson <sandraprinson31@gmail.com>
2023-10-02 13:17:00 +0530

    closure

--- script.js
+++ script.js
@@ -193,17 +193,33 @@
 
 // ------------clousure----------
 
+var objs = [
 
-function add (a ,b){
-
-    const c =a+b
-    return c;
-}
+   { name: 'I',
+    msg : 'hi...i,m sandra'
+},
+{
+    name:'About',
+    msg :'web developer'
+},
+];
 
 function main()
 {
-    const res = add(3,4)
-    console.log(res)
+    objs.forEach((item)=>
+    {
+        const bt = document.createElement('button');
+        bt.innerHTML =item.name;
+        bt.onclick = getCallBack(item.msg);
+        document.body.appendChild(bt);
+
+    });
 }
 
+function getCallBack(arg) {
+    return function()
+    {
+        alert(arg);
+    }
+}
 main();
